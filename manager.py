09e72c6f149a91bb0262884cbd983840afb727c4
waltergu <waltergu1989@gmail.com>
2018-07-16 16:06:00 +0800

    A small step toward goal. Maybe filling factor should be changed.

--- manager.py
+++ manager.py
@@ -9,18 +9,28 @@ def tbatasks(name,parameters,lattice,terms,boundaries,statistics,jobs=()):
     tba=tbaconstruct(name,parameters,lattice,terms,boundaries,statistics)
     #tba.generator.view(suspend=True)
     if 'EB' in jobs:
-        path=hexagon_gkm(reciprocals=lattice.reciprocals,nk=100)
+        if len(lattice.vectors)==2:
+            path=hexagon_gkm(reciprocals=lattice.reciprocals,nk=100)
+            #path=KPath(KMap(lattice.reciprocals,'H:G-K,K-M,M-G'),nk=100)
+        elif len(lattice.vectors)==1:
+            path=line_bz(reciprocals=lattice.reciprocals,nk=400)
+        else:
+            path=None
         tba.register(EB(name='EB',path=path,run=TBA.TBAEB))
         tba.summary()
-    if 'GSE' in jobs:
-        tba.register(TBA.GSE(filling=0.1,run=TBA.TBAGSE))
+    if 'GR' in jobs:
+        path=KSpace(reciprocals=lattice.reciprocals,nk=50)
+        tba.register(EB(name='GR',path=path,plot=False,savedata=False,run=TBA.TBAEB))
+        gap=tba.records['GR'][:,2].min()-tba.records['GR'][:,1].max()
+        bandwidth=tba.records['GR'][:,1].max()-tba.records['GR'][:,1].min()
+        tba.log<<'gap,ratio: %s, %s\n'%(gap,gap/bandwidth)
         tba.summary()
 
 def edtasks(name,parameters,basis,lattice,terms,boundaries,statistics,jobs=()):
     import HamiltonianPy.ED as ED
     ed=edconstruct(name,parameters,[basis],lattice,terms,boundaries,statistics)
     if 'EL1' in jobs: ed.register(ED.EL(name='EL1',path=BaseSpace(('theta1',np.linspace(0,1.0,21))),ns=15,run=ED.EDEL))
-    if 'EL2' in jobs: ed.register(ED.EL(name='EL2',path=BaseSpace(('theta2',np.linspace(0,1.0,21))),ns=10,run=ED.EDEL))
+    if 'EL2' in jobs: ed.register(ED.EL(name='EL2',path=BaseSpace(('theta2',np.linspace(0,1.0,21))),ns=15,run=ED.EDEL))
     if 'ELU' in jobs: ed.register(ED.EL(name='EL2',path=BaseSpace(('U',np.linspace(0,20.0,21))),ns=1,nder=2,run=ED.EDEL))
     if 'EIGS' in jobs: ed.register(ED.EIGS(name='EIGS',ne=10,run=ED.EDEIGS))
     ed.summary()
@@ -35,26 +45,30 @@ if __name__=='__main__':
     parameters['t']=1.0
 
     # tba
-    #tbatasks(namef,parameters,T1('1P-1P',nnb),[t],None,'f',jobs=['EB'])
+    #tbatasks(namef,parameters,T1('1P-1P',nnb),[t,h],None,'f',jobs=['EB'])
+    #tbatasks(namef,parameters,T1('1P-20O',nnb),[t,h],None,'f',jobs=['EB'])
+    #tbatasks(namef,parameters,T1('1P-1P',nnb),[t,h],None,'f',jobs=['GAP'])
 
     # ed
-    parameters['U']=20.0
-    parameters['V']=5.0
+    parameters['U']=120.0
+    parameters['V1']=40.0
+    parameters['V2']=40.0
+    parameters['V3']=40.0
     parameters['theta1']=0.0
     parameters['theta2']=0.0
 
-    m,n,filling=4,5,10
+    m,n,filling=3,5,15
     lattice=T1('%sP-%sP'%(m,n),nnb)
     boundaries=[('theta1','theta2'),(0.0,0.0),lattice.vectors,twisttransformation]
 
     # fermi
-    #edtasks(namef,parameters,FBasis(m*n*2,m*n*2/filling),lattice,[t,U,V],boundaries,'f',jobs=['EL1'])
-    #edtasks(namef,parameters,FBasis(m*n*2,m*n*2/filling),lattice,[t,U,V],boundaries,'f',jobs=['EL2'])
-    #edtasks(namef,parameters,FBasis(m*n*2,m*n*2/filling),lattice,[t,U,V],boundaries,'f',jobs=['ELU'])
-    #edtasks(namef,parameters,FBasis(m*n*2,m*n*2/filling),lattice,[t,U,V],boundaries,'f',jobs=['EIGS'])
+    #edtasks(namef,parameters,FBasis(m*n*2,m*n*2/filling),lattice,[t,U,V1,V2],boundaries,'f',jobs=['EL1'])
+    #edtasks(namef,parameters,FBasis(m*n*2,m*n*2/filling),lattice,[t,U,V1,V2,V3],boundaries,'f',jobs=['EL2'])
+    #edtasks(namef,parameters,FBasis(m*n*2,m*n*2/filling),lattice,[t,U,V1,V2],boundaries,'f',jobs=['ELU'])
+    #edtasks(namef,parameters,FBasis(m*n*2,m*n*2/filling),lattice,[t,U,V1,V2],boundaries,'f',jobs=['EIGS'])
 
     # bose
-    #edtasks(nameb,parameters,FBasis(m*n*2,m*n*2/filling),lattice,[t,U,V],boundaries,'b',jobs=['EL1'])
-    #edtasks(nameb,parameters,FBasis(m*n*2,m*n*2/filling),lattice,[t,U,V],boundaries,'b',jobs=['EL2'])
-    #edtasks(nameb,parameters,FBasis(m*n*2,m*n*2/filling),lattice,[t,U,V],boundaries,'b',jobs=['ELU'])
-    #edtasks(nameb,parameters,FBasis(m*n*2,m*n*2/filling),lattice,[t,U,V],boundaries,'b',jobs=['EIGS'])
+    #edtasks(nameb,parameters,FBasis(m*n*2,m*n*2/filling),lattice,[t,U,V1,V2],boundaries,'b',jobs=['EL1'])
+    #edtasks(nameb,parameters,FBasis(m*n*2,m*n*2/filling),lattice,[t,U,V1,V2],boundaries,'b',jobs=['EL2'])
+    #edtasks(nameb,parameters,FBasis(m*n*2,m*n*2/filling),lattice,[t,U,V1,V2],boundaries,'b',jobs=['ELU'])
+    #edtasks(nameb,parameters,FBasis(m*n*2,m*n*2/filling),lattice,[t,U,V1,V2],boundaries,'b',jobs=['EIGS'])
--- source/config.py
+++ source/config.py
@@ -2,12 +2,12 @@ from HamiltonianPy import *
 from numpy.linalg import inv
 import numpy as np
 
-__all__=['nameb','namef','nnb','parametermap','idfmap','t','U','V','T1']
+__all__=['nameb','namef','nnb','parametermap','idfmap','t','h','U','V1','V2','V3','T1']
 
 # The configs of the model
 nameb='BTKH'
 namef='FTKH'
-nnb=1
+nnb=3
 
 # parametermap
 parametermap=None
@@ -22,10 +22,18 @@ def kitaev(bond):
     if abs(theta-60)<RZERO or abs(theta-240)<RZERO: return sigmay('sp')
     if abs(theta-120)<RZERO or abs(theta-300)<RZERO: return sigmaz('sp')
 
+# magnetic field
+def magnetic(bond,theta,phi):
+    theta,phi=theta*np.pi,phi*np.pi*2
+    return sigmax('sp')*np.sin(theta)*np.cos(phi)+sigmay('sp')*np.sin(theta)*np.sin(phi)+sigmaz('sp')*np.cos(theta)
+
 # terms
 t=lambda statistics,**parameters: Hopping('t',parameters['t'],indexpacks=kitaev,statistics=statistics)
+h=lambda statistics,**parameters: Onsite('h',parameters['h'],indexpacks=lambda bond: magnetic(bond,parameters['theta'],parameters['phi']),statistics=statistics)
 U=lambda statistics,**parameters: Hubbard('U',parameters['U'],statistics=statistics,modulate=True)
-V=lambda statistics,**parameters: Coulomb('V',parameters['V'],neighbour=1,statistics=statistics,modulate=True)
+V1=lambda statistics,**parameters: Coulomb('V1',parameters['V1'],neighbour=1,statistics=statistics,modulate=True)
+V2=lambda statistics,**parameters: Coulomb('V2',parameters['V2'],neighbour=2,statistics=statistics,modulate=True)
+V3=lambda statistics,**parameters: Coulomb('V3',parameters['V3'],neighbour=3,statistics=statistics,modulate=True)
 
 # cluster
 T1=Triangle('T1')
